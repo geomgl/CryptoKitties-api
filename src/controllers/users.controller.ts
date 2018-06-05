0a603be98bcc150a3641eb03b2b846bd4733cbff
John Bracaglia <jabracaglia@Johns-MacBook-Pro-2.local>
2018-06-05 10:18:27 +0200

    third version

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,17 +2,30 @@
 
 import { repository } from "@loopback/repository";
 import { UserRepository } from "../repositories/user.repository";
-import { post, get, requestBody } from "@loopback/rest";
+import { post, get, requestBody, param, HttpErrors } from "@loopback/rest";
 import { User } from "../models/user";
 
 
-export class UsersController {
-  @repository(UserRepository.name) private userRepo: UserRepository
-  constructor() {}
+export class UsersController { 
+  constructor(
+    @repository(UserRepository) private userRepo: UserRepository
+  ) {}
 
   @get('/users')
-  async getAllUsers(): Promise<Array<User>> {
+  async findUsers(): Promise<User[]> {
   return await this.userRepo.find();
+  }
+
+  @get('/users/{id}')
+  async findUsersById(@param.path.number('id') id: number): Promise<User> {
+    let userExists: boolean = !!(await this.userRepo.count({ id }));
+
+    if (!userExists) {
+      throw new HttpErrors.BadRequest('ID ${id} does not exist');
+    }
+
+    return await this.userRepo.findById(id);
+
   }
   
 }
