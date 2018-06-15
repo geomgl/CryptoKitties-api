6a7cc1c2bafbcc9e5219c350a998db5b539a0ec6
John Bracaglia <jabracaglia@gmail.com>
2018-06-15 11:07:11 +0200

    update gitignore; gitignore dist directory

--- dist8/src/controllers/users.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserRepository } from "../repositories/user.repository";
-import { User } from "../models/user";
-export declare class UsersController {
-    private userRepo;
-    constructor(userRepo: UserRepository);
-    findUsers(): Promise<User[]>;
-    findUsersById(id: number): Promise<User>;
-    getDonationsByUserId(userId: number, dateFrom: Date): Promise<void>;
-}
--- dist8/src/models/charity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Entity } from '@loopback/repository';
-export declare class Charity extends Entity {
-    id?: number;
-    name: string;
-    mission?: string;
-    description?: string;
-    button?: string;
-}
